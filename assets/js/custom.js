549eb8eadcb1cb6b027cb4340116c969f1132e1d
gyan prakash verma <gyan.prakash@jogaapp.com>
2020-09-30 15:39:04 +0530

    changes

---
+++ assets/js/custom.js
@@ -0,0 +1,162 @@
+
+$(document).ready(function(){
+    // AOS animation 
+    AOS.init({
+        easing: 'ease-out-back',
+        duration: 1000,
+        disable: false,
+    });
+    
+    // Back To Top Button
+    var backButton = $('.back-to-top');
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 300) {
+          backButton.addClass('shown');
+        } else {
+          backButton.removeClass('shown');
+        }
+    });
+    
+    backButton.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, 1000);
+    });
+});
+
+// Partical JS
+/* ---- particles.js config ---- */
+
+particlesJS("particles-js", {
+    particles: {
+        number: {
+            value: 150,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        },
+        color: {
+            value: "#fff"
+        },
+        shape: {
+            type: "circle",
+            stroke: {
+                width: 0,
+                color: "#fff"
+            },
+            polygon: {
+                nb_sides: 51
+            },
+            image: {
+                src: "img/github.svg",
+                width: 80,
+                height: 80
+            }
+        },
+        opacity: {
+            value: 0.5,
+            random: false,
+            anim: {
+                enable: false,
+                speed: 5,
+                opacity_min: 0.1,
+                sync: false
+            }
+        },
+        size: {
+            value: 3,
+            random: true,
+            anim: {
+                enable: false,
+                speed: 100,
+                size_min: 0.1,
+                sync: false
+            }
+        },
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#fff",
+            opacity: 0.4,
+            width: 1
+        },
+        move: {
+            enable: true,
+            speed: 6,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200
+            }
+        }
+        },
+        interactivity: {
+            detect_on: "canvas",
+            events: {
+                onhover: {
+                enable: true,
+                mode: "grab"
+            },
+            onclick: {
+                enable: true,
+                mode: "push"
+            },
+                resize: true
+        },
+        modes: {
+            grab: {
+                distance: 140,
+                line_linked: {
+                    opacity: 1
+                }
+            },
+            bubble: {
+                distance: 400,
+                size: 60,
+                duration: 2,
+                opacity: 8,
+                speed: 3
+            },
+            repulse: {
+                distance: 200,
+                duration: 0.4
+            },
+            push: {
+                particles_nb: 4
+            },
+            remove: {
+                particles_nb: 2
+            }
+        }
+    },
+    retina_detect: true
+});
+
+/* ---- stats.js config ---- */
+
+var count_particles, stats, update;
+stats = new Stats();
+stats.setMode(0);
+stats.domElement.style.position = "absolute";
+stats.domElement.style.left = "0px";
+stats.domElement.style.top = "0px";
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector(".js-count-particles");
+update = function () {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
+
+
+
+    
